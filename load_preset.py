2b569b9ed47556115214d6abc862a4802b34a303
tonton <samytichadou@gmail.com>
2023-03-03 10:24:53 +0100

    Update changes after loading preset

--- load_preset.py
+++ load_preset.py
@@ -9,22 +9,23 @@ def set_input(preset_input, ng_input):
 
 def set_nodegroup_inputs(preset, modifier):
     for input in preset.inputs:
-        print(input.name)
+        #print(input.name)
         chk=0
         for i in modifier.node_group.inputs:
             if i.name==input.name and i.identifier==input.identifier:
-                print(f"match : {i.name}")
+                #print(f"match : {i.name}")
                 if input.type in ("VECTOR","RGBA"):
                     for n in range(0,len(modifier[i.identifier])):
                         modifier[i.identifier][n]=getattr(input, input.type.lower())[n]
                 else:
+                    #setattr(modifier, i.identifier, getattr(input, input.type.lower()))
                     modifier[i.identifier]=getattr(input, input.type.lower())
                 chk=1
                 break
         if chk==0:
             for i in modifier.node_group.inputs:
                 if i.name==input.name:
-                    print(f"semimatch : {i.name}")
+                    #print(f"semimatch : {i.name}")
                     if input.type in ("VECTOR","RGBA"):
                         for n in range(0,len(modifier[i.identifier])):
                             modifier[i.identifier][n]=getattr(input, input.type.lower())[n]
@@ -51,6 +52,9 @@ class GNPRESET_OT_load_preset(bpy.types.Operator):
         preset=mod.node_group.gnpreset_presets[self.preset_name]
 
         set_nodegroup_inputs(preset, mod)
+
+        # Update object modifications
+        context.object.data.update()
 
         self.report({'INFO'}, f"Preset {self.preset_name} loaded")
 
