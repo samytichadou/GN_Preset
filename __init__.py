948cb564aa41fe0681b8d531f767f90100bee207
tonton <samytichadou@gmail.com>
2023-03-03 12:11:29 +0100

    bump version

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "name": "GN Preset",
     "description": "Create Geometry Nodes presets",
     "author": "Samy Tichadou (tonton)",
-    "version": (0, 1, 1),
+    "version": (0, 1, 2),
     "blender": (3, 0, 0),
     "location": "Modifier Panel",
     "wiki_url": "https://github.com/samytichadou/GN_Preset/blob/master/README.md",
